524105c6c628c7c6b9cedf7ac2966f927f23c5f2
Rukshanarupu <121860594+Rukshanarupu@users.noreply.github.com>
2023-07-24 13:47:08 +0600

    Update Admission.jsx

--- uniPlanet-client/src/Pages/Admission/Admission.jsx
+++ uniPlanet-client/src/Pages/Admission/Admission.jsx
@@ -2,6 +2,12 @@ import { Link, useLoaderData } from "react-router-dom";
 
 const Admission = () => {
   const admissionDetailsLoaded = useLoaderData();
+  const [selectedCollege, setSelectedCollege] = useState(null);
+
+  const handleCollegeClick = (admissionDetail) => {
+    setSelectedCollege(admissionDetail);
+  };
+  
   console.log(admissionDetailsLoaded)
  
   return (
@@ -14,32 +20,14 @@ const Admission = () => {
               <h2 className="card-title">{admissionDetail.name}</h2>
               <p>Please click the button to booking your expected College.</p>
               <div className="card-actions justify-end">
-                <Link to={`/admissionDetails/${_id}`} className="btn bg-primary normal-case text-black hover:bg-secondary border-0">Booking</Link>
+                <button onClick={() => handleCollegeClick(admissionDetail)}> className="btn bg-primary normal-case text-black hover:bg-secondary border-0">Select</button>
               </div>
             </div>
           </div>
         ))
       }
-
     </div>
   );
 };
 
-
-{/* <div className="card w-96 bg-base-100 shadow-xl image-full">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div> */}
-// { 
-//   sports_facility.facilities.map((facility, _id) => (
-//       <div key={_id}>
-//           <p>{facility}</p>
-//       </div>
-//   ))}
 export default Admission;
